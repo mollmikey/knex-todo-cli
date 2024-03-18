156541b1ae2ef31dd105257413259aebb065a3d8
Michael <104609759+mollmikey@users.noreply.github.com>
2024-03-18 15:13:11 +1300

    9-Add complete column to dataTable

---
+++ migrations/20240318015517_add-completed-column.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.alterTable('todos', (table) => {
+    table.boolean('complete')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.alterTable('todos', (table) => {
+    table.dropColumn('complete')
+  })
+}
